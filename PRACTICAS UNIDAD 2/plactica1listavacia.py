9a356b4206ed45ccbd804b647f98604113db56e0
Emmanuel Castruita Soto <a.23308051281176@cbtis128.edu.mx>
2024-10-17 16:14:58 -0600

    Add files via upload

---
+++ PRACTICAS UNIDAD 2/plactica1listavacia.py
@@ -0,0 +1,33 @@
+print("")  # Imprime una línea en blanco.
+print("CASTRUITA SOTO EMMANUEL 3-W 1176")  # Imprime el nombre completo y un código.
+
+print("")  # Imprime otra línea en blanco.
+diccionario = {}  # Inicializa un diccionario vacío para almacenar datos.
+nombre = input("ingresa tu nombre")  # Pide al usuario que ingrese su nombre.
+diccionario["nombre="] = nombre  # Almacena el nombre en el diccionario.
+print(diccionario)  # Imprime el diccionario con el nombre incluido.
+
+edad = input("ingresa tu edad")  # Pide al usuario que ingrese su edad.
+diccionario["edad="] = edad  # Almacena la edad en el diccionario.
+print(diccionario)  # Imprime el diccionario con la edad incluida.
+
+sexo = input("ingresa tu sexo")  # Pide al usuario que ingrese su sexo.
+diccionario["sexo="] = sexo  # Almacena el sexo en el diccionario.
+print(diccionario)  # Imprime el diccionario con el sexo incluido.
+
+telefono = input("ingresa tu telefono")  # Pide al usuario que ingrese su teléfono.
+diccionario["telefono="] = telefono  # Almacena el teléfono en el diccionario.
+# Se podría añadir más datos como nombre, edad, sexo, teléfono, correo electrónico, etc.
+print(diccionario)  # Imprime el diccionario con el teléfono incluido.
+
+correoelectronico = input("ingresa tu correoelectronico")  # Pide el correo electrónico.
+diccionario["correoelectronico="] = correoelectronico  # Almacena el correo en el diccionario.
+print(diccionario)  # Imprime el diccionario con el correo incluido.
+
+estatura = input("ingresa tu estatura")  # Pide al usuario que ingrese su estatura.
+diccionario["mides="] = estatura  # Almacena la estatura en el diccionario.
+print(diccionario)  # Imprime el diccionario con la estatura incluida.
+
+frutafavorita = input("ingresa tu fruta favorita")  # Pide al usuario que ingrese su fruta favorita.
+diccionario["fruta favorita="] = frutafavorita  # Almacena la fruta favorita en el diccionario.
+print(diccionario)  # Imprime el diccionario final con todos los datos.
